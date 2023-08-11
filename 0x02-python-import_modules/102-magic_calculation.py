2356910f3c9d0c7693f13c4c1e4fe3376dd13f5b
VictorMaona <maonavictor@gmail.com>
2023-08-11 14:09:08 -0700

    update 102

--- 0x02-python-import_modules/102-magic_calculation.py
+++ 0x02-python-import_modules/102-magic_calculation.py
@@ -1,7 +1,7 @@
 #!/usr/bin/python3
 
 def magic_calculation(a, b):
-    """match bytecode that Holberton School has provided."""
+    """Bytecode that Holberton School provided."""
     from magic_calculation_102 import add, sub
 
     if a < b:
