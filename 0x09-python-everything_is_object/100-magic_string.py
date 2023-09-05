051c1f868baf1f22b92d0c6fc804a75e00c6fb0a
VictorMaona <maonavictor@gmail.com>
2023-09-05 14:17:22 -0700

    100-magic_string.py update

--- 0x09-python-everything_is_object/100-magic_string.py
+++ 0x09-python-everything_is_object/100-magic_string.py
@@ -1,17 +1,7 @@
 #!/usr/bin/python3
 
 def magic_string():
-    # Get the word "BestSchool" n times in a string.
-    if hasattr(magic_string, 'calls'):
-        magic_string.calls += 1
-    else:
-        magic_string.calls = 1
-
-    return ', '.join(['BestSchool'] * magic_string.calls)
-
-# Original response
-#
-# #!/usr/bin/python3
-# def magic_string():
-#     setattr(magic_string, 'rep', getattr(magic_string, 'rep', -1) + 1)
-#     return 'BestSchool' + ', BestSchool' * getattr(magic_string, 'rep', 0)
+    if not hasattr(magic_string, "count"):
+        magic_string.count = 0
+    magic_string.count += 1
+    return "BestSchool" + ", BestSchool" * (magic_string.count - 1)
