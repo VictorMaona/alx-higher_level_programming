c6fe869413279832906406f1f353b7f0860129b0
VictorMaona <maonavictor@gmail.com>
2023-09-06 14:23:34 -0700

    0x08. Python - More Classes and Objects final update

--- 0x08-python-more_classes/6-rectangle.py
+++ 0x08-python-more_classes/6-rectangle.py
@@ -12,7 +12,7 @@ class Rectangle:
     number_of_instances = 0
 
     def __init__(self, width=0, height=0):
-        """Initialize a new Rectangle.
+        """start a new Rectangle.
 
         Args:
             width (int): The new rectangle width.
