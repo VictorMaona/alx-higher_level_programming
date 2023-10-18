109e53f4c53640a98bdf2d4513ed0f608a863ca2
VictorMaona <maonavictor@gmail.com>
2023-10-18 16:58:00 -0700

    updated

--- 0x0E-SQL_more_queries/5-unique_id.sql
+++ 0x0E-SQL_more_queries/5-unique_id.sql
@@ -1,5 +1,2 @@
--- Create or alter the unique_id table.
-CREATE TABLE IF NOT EXISTS unique_id (
-    id INT NOT NULL AUTO_INCREMENT UNIQUE DEFAULT 1,
-    name VARCHAR(256)
-);
+-- Create or alter the unique_id table
+CREATE TABLE IF NOT EXISTS unique_id (id INT DEFAULT 1 UNIQUE, name VARCHAR(256));
