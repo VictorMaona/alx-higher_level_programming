f5c79dca4bbec2382ddd529864b5afbd325c85f5
VictorMaona <maonavictor@gmail.com>
2023-11-14 03:39:57 -0800

    updated

--- 0x13-javascript_objects_scopes_closures/3-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/3-rectangle.js
@@ -1,24 +1,11 @@
 #!/usr/bin/node
+// Verify if w and h are both positive integers.
 module.exports = class Rectangle {
-  /**
-   * The Rectangle class constructor.
-   * @param {number} w - Width of the rectangle.
-   * @param {number} h - Height of the rectangle.
-   */
-  constructor(w, h) {
-    // Verify if w and h are both positive integers.
-    if (w > 0 && h > 0) {
-      // Assignment of destructuring to initialize width and height
-      [this.width, this.height] = [w, h];
-    }
+  constructor (w, h) {
+    if (w > 0 && h > 0) { [this.width, this.height] = [w, h]; }
   }
 
-  /**
-   * Instance method to print the rectangle with 'X' character.
-   */
-  print() {
-    for (let i = 0; i < this.height; i++) {
-      console.log('X'.repeat(this.width));
-    }
+  print () {
+    for (let i = 0; i < this.height; i++) console.log('X'.repeat(this.width));
   }
 };
