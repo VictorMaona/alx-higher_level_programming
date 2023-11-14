13df803c77715d0c949cf9ea93f14b742eae591d
VictorMaona <maonavictor@gmail.com>
2023-11-14 02:56:16 -0800

    updated

--- 0x13-javascript_objects_scopes_closures/2-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/2-rectangle.js
@@ -5,7 +5,6 @@ module.exports = class Rectangle {
   constructor(w, h) {
     // Verify if w and h are both positive integers.
     if (w > 0 && h > 0) {
-      // Assignment of destructuring to initialize the width and height properties
       [this.width, this.height] = [w, h];
     }
   }
