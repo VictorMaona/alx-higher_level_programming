df5a2252d0eac215cccca32a34a86bfce05b8d53
VictorMaona <maonavictor@gmail.com>
2023-11-14 02:28:11 -0800

    updated

--- 0x13-javascript_objects_scopes_closures/1-rectangle.js
+++ 0x13-javascript_objects_scopes_closures/1-rectangle.js
@@ -2,7 +2,6 @@
 
 // Describe the class Rectangle.
 module.exports = class Rectangle {
-  // Constructor acceptable parameters for height and width
   constructor(w, h) {
     this.width = w;
     this.height = h;
