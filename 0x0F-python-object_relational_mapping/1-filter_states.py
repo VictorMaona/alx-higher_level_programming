678895f585218d11975509765b0e86092014d7ff
VictorMaona <maonavictor@gmail.com>
2023-11-17 04:02:20 -0800

    updted

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,36 +1,51 @@
 #!/usr/bin/python3
-""" 
-Connect to the MySQL database and retrieve states.
-Display states with names starting with 'N' (case-sensitive).
-"""
+"""Lists states with a name starting with 'N' (case-sensitive) from the database hbtn_0e_0_usa"""
 
 import MySQLdb
 import sys
 
 if __name__ == "__main__":
-    # Connect to the MySQL database
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    
-    # Create a cursor object to interact with the database
-    cur = db.cursor()
+    # Check if the correct number of command-line arguments is provided
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
 
-    # Execute the SQL query to select states with names starting
-    # with 'N' in a case-sensitive manner, and order the results
-    # by the 'id' field in ascending order
-    cur.execute("""
-        SELECT * FROM states
-        WHERE name LIKE BINARY 'N%'
-        ORDER BY states.id
-    """)
+    # Extract command-line arguments
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
 
-    # Fetch all the rows
-    rows = cur.fetchall()
+    try:
+        # Connect to the MySQL database
+        db = MySQLdb.connect(host="localhost", user=username, passwd=password, db=database, port=3306)
 
-    # Print the rows (states) one by one
-    for row in rows:
-        print(row)
+        # Create a cursor object to interact with the database
+        cursor = db.cursor()
 
-    # Close the cursor and database connection to release resources
-    cur.close()
-    db.close()
+        # Execute the SQL query to select states with names starting with 'N' (case-sensitive)
+        query = """
+                SELECT * FROM states
+                WHERE name LIKE BINARY 'N%'
+                ORDER BY states.id
+                """
+        cursor.execute(query)
+
+        # Fetch all the rows
+        rows = cursor.fetchall()
+
+        # Print the rows (states) one by one
+        for row in rows:
+            print(row)
+
+    except MySQLdb.Error as e:
+        # Handle MySQL errors
+        print("MySQL Error: {}".format(e))
+
+    except Exception as e:
+        # Handle other exceptions
+        print("Error: {}".format(e))
+
+    finally:
+        # Close the cursor and database connection to release resources
+        if cursor:
+            cursor.close()
+        if db:
+            db.close()
