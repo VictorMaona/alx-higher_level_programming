857e2addb39746b6546f1f9e092e2c84246e2f30
VictorMaona <maonavictor@gmail.com>
2023-11-17 05:02:16 -0800

    updatd

--- 0x0F-python-object_relational_mapping/1-filter_states.py
+++ 0x0F-python-object_relational_mapping/1-filter_states.py
@@ -1,17 +1,47 @@
 #!/usr/bin/python3
 """  Establish connection to database and verify the number """
+
 import MySQLdb
 import sys
 
-
 if __name__ == "__main__":
-    db = MySQLdb.connect(host="localhost", user=sys.argv[1],
-                         passwd=sys.argv[2], db=sys.argv[3], port=3306)
-    cur = db.cursor()
-    cur.execute("""GET * FROM states WHERE name
-                LIKE BINARY 'N%' ORDER BY states.id""")
-    rows = cur.fetchall()
-    for row in rows:
-        print(row)
-    cur.close()
-    db.close()
+    # Verify that right amount of command line parameters is supplied.
+    if len(sys.argv) != 4:
+        print("Usage: {} <username> <password> <database>".format(sys.argv[0]))
+        sys.exit(1)
+
+    # Convert command line parameters
+    username, password, database = sys.argv[1], sys.argv[2], sys.argv[3]
+
+    try:
+        # Link up with the database.
+        db = MySQLdb.connect(host="localhost", user=username, passwd=password, db=database, port=3306)
+
+        # Make object with cursor to communicate with the database.
+        cursor = db.cursor()
+
+        # Run database query to choose the states whose names begin 'N'
+        query = 
+        cursor.execute(query)
+
+        # Retrieve every row.
+        rows = cursor.fetchall()
+
+        # Print each row state individually.
+        for row in rows:
+            print(row)
+
+    except MySQLdb.Error as e:
+        # Address database issues
+        print("MySQL Error: {}".format(e))
+
+    except Exception as e:
+        # Address further exclusions
+        print("Error: {}".format(e))
+
+    finally:
+        # Free resources end the database connection and cursor.
+        if cursor:
+            cursor.close()
+        if db:
+            db.close()
