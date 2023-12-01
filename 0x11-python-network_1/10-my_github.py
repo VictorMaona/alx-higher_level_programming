6a4a9e0c67bdbf654a750029040dc45d4564108d
VictorMaona <maonavictor@gmail.com>
2023-12-01 12:13:23 -0800

    updated

--- 0x11-python-network_1/10-my_github.py
+++ 0x11-python-network_1/10-my_github.py
@@ -5,6 +5,28 @@ accesses information using personal access token in conjunction with Basic Authe
 and shows your user ID on GitHub.
 makes use of requests library
 """
-import sys
+
 import requests
-from requests.auth import HTTPBasicAuth
+import sys
+
+if len(sys.argv) == 3:
+    username = sys.argv[1]
+    password = sys.argv[2]
+
+    # Describe user information endpoint for GitHub API.
+    url = 'https://api.github.com/user'
+
+    # Utilize personal access token to configure Basic Authentication.
+    auth = (username, password)
+
+    # Sends the GitHub API a GET request.
+    response = requests.get(url, auth=auth)
+
+    # Verify whether the request was fulfilled.
+    if response.status_code == 200:
+        user_data = response.json()
+        print(user_data.get('id'))
+    else:
+        print(None)
+else:
+    print("Usage: ./10-my_github.py <username> <personal_access_token>")
