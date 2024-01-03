e781dc400d98b8e99ceac8e460dc129f605e3583
VictorMaona <maonavictor@gmail.com>
2024-01-03 09:02:01 -0800

    updated

--- 0x14-javascript-web_scraping/6-completed_tasks.js
+++ 0x14-javascript-web_scraping/6-completed_tasks.js
@@ -1,27 +1,21 @@
 #!/usr/bin/node
-
-const request = require('request');
-const url = process.argv[2];
-
 // Send a request to the API URL provided.
-request(url, function (error, response, body) {
-  if (err) {
-    console.log(err);
-  } else if (response.statusCode === 200) {
-    const completed = {};
-    const tasks = JSON.parse(body);
-    for (const i in tasks) {
-      const task = tasks[i];
-      if (task.completed === true) {
-        if (completed[task.userId] === undefined) {
-          completed[task.userId] = 1;
-        } else {
-          completed[task.userId]++;
-        }
+const request = require('request');
+
+request(process.argv[2], function (error, response, body) {
+  if (error) {
+    console.error(error);
+  }
+  const taskList = JSON.parse(body);
+  const userTotals = {};
+  for (const task of taskList) {
+    if (task.completed) {
+      if (task.userId in userTotals) {
+        userTotals[task.userId] += 1;
+      } else {
+        userTotals[task.userId] = 1;
       }
     }
-    console.log(completed);
-  } else {
-    console.log('An error occured. Status code: ' + response.statusCode);
   }
+  console.log(userTotals);
 });
