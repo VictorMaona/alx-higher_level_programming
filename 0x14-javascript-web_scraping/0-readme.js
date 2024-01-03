9fe56caa8cca0a094572d8ba91bbdca81b57d9da
VictorMaona <maonavictor@gmail.com>
2024-01-03 04:15:20 -0800

    updated

--- 0x14-javascript-web_scraping/0-readme.js
+++ 0x14-javascript-web_scraping/0-readme.js
@@ -1,13 +1,12 @@
 #!/usr/bin/node
 
-// FS module import for file system functions
-const fs = require('fs');
-const file = process.argv[2];
+const request = require('request');
+const URL = process.argv[2];
 
-fs.readFile(file, 'utf-8', function (err, data) {
+request(URL, function (err, response) {
   if (err) {
     console.log(err);
   } else {
-    console.log(data);
+    console.log('code: ' + response.statusCode);
   }
 });
