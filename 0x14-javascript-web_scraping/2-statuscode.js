b75ee21fd6844f5efdfa264c50680fb055a0b8b8
VictorMaona <maonavictor@gmail.com>
2024-01-03 05:07:32 -0800

    updated

---
+++ 0x14-javascript-web_scraping/2-statuscode.js
@@ -0,0 +1,13 @@
+#!/usr/bin/node
+
+// FS module import for file system functions
+const fs = require('fs');
+const URL = process.argv[2];
+
+request(URL, function (err, response) {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('code: ' + response.statusCode);
+  }
+});
